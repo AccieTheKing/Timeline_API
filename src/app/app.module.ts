8db98ab054ea83118c27d1e726ac5d47b9722b7b
Acdaling <acdaling@gmail.com>
2021-08-17 17:15:06 +0200

    Added milestone module to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from 'src/users/user.module';
 import { BoardModule } from 'src/boards/board.module';
+import { MilestoneModule } from 'src/milestones/milestone.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { BoardModule } from 'src/boards/board.module';
     ),
     UserModule,
     BoardModule,
+    MilestoneModule,
   ],
   controllers: [AppController],
   providers: [AppService],
