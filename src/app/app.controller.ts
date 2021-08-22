7c82cd5697d4601013b9526c62eb72c803fc29c2
Acdaling <acdaling@gmail.com>
2021-08-22 23:44:48 +0200

    Successful login will return a jwt token to the user

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
 import { LocalAuthGuard } from 'src/auth/local-auth.guard';
 
 @Controller()
 export class AppController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req) {
-    return req.user;
+  async login(@Request() req): Promise<{ access_token: string }> {
+    return this.authService.login(req.user); // Return JWT-token to the user
+  }
+
   }
 }
