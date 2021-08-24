39a4518a8155e38c9ceaf79a8a5ee0fe110d3e10
Acdaling <acdaling@gmail.com>
2021-08-24 18:00:52 +0200

    Cleared Auth references from app controller

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,22 +1,6 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthService } from 'src/auth/auth.service';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { LocalAuthGuard } from 'src/auth/local-auth.guard';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
-
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req): Promise<{ access_token: string }> {
-    return this.authService.login(req.user); // Return JWT-token to the user
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('auth/protected')
-  async protectedTestRoute(@Request() req) {
-    // Require a bearer token to access
-    return req.user;
-  }
+  constructor() {}
 }
