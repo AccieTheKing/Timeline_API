9ba691e50a037ea6f65aa02a4c86f4aa27bf15f3
Acdaling <acdaling@gmail.com>
2021-08-24 20:39:25 +0200

    Created a route for registing a new user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { User, UserDocument } from 'src/schemas/user.schema';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './localAuthentication.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async registerUser(
+    @Body() registrationData: User,
+  ): Promise<Partial<UserDocument>> {
+    return this.authService.registerUser({
+      ...registrationData,
+      subscriptionType: 'FREE',
+      numberOfBoards: 0,
+    });
+  }
 }
