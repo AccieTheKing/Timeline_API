c8b4153edcb34b379a3f06ade2016a2328fdb314
Acdaling <acdaling@gmail.com>
2021-08-26 15:12:39 +0200

    Created jwt strategy for extracting Authentication token from cookies
    Jwt strategy for returning user object when local auth is used

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express-serve-static-core';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User, UserDocument } from 'src/schemas/user.schema';
+import { UserService } from 'src/users/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request?.cookies?.Authentication;
+        },
+      ]),
+      secretOrKey: `${configService.get<string>('JWT_REFRESH_TOKEN_SECRET')}`,
+    });
+  }
+
+  async validate(payload: { username: string }): Promise<User> {
+    const { username, numberOfBoards, subscriptionType } =
+      await this.userService.findUser(payload.username);
+    return { username, numberOfBoards, subscriptionType };
+  }
+}
