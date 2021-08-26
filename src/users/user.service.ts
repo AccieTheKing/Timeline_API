b8fc7ee688ecee9889259b768f7320ca2b0bacf7
Acdaling <acdaling@gmail.com>
2021-08-26 15:40:28 +0200

    Created methods for storing and fetching the refresh token from user

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { compareHash, createHash } from 'src/helpers/hashing';
 import { UserDocument, User } from 'src/schemas/user.schema';
 
 @Injectable()
@@ -28,5 +29,56 @@ export class UserService {
 
   async deleteUser(username: string): Promise<UserDocument> {
     return this.userModel.findOneAndDelete({ username });
+  }
+
+  /**
+   * This method is for storing the refresh token in the user schema
+   *
+   * @param refreshToken
+   * @param username
+   */
+  async setCurrentRefreshToken(
+    refreshToken: string,
+    username: string,
+  ): Promise<void> {
+    const currentRefreshToken = await createHash(refreshToken);
+    this.userModel.findOneAndUpdate(
+      { username },
+      {
+        refreshToken: currentRefreshToken,
+      },
+    );
+  }
+
+  /**
+   * This method will verify the refresh token stored with the user schema in the database
+   *
+   * @param refreshToken
+   * @param username
+   * @returns
+   */
+  async getUserIfRefreshTokenMatches(
+    refreshToken: string,
+    username: string,
+  ): Promise<UserDocument> {
+    const user: UserDocument = await this.findUser(username);
+    const isRefreshTokenValid: boolean = await compareHash(
+      refreshToken,
+      user.refreshToken,
+    );
+
+    if (isRefreshTokenValid) {
+      return user;
+    }
+  }
+
+  async removeRefreshToken(username: string): Promise<UserDocument> {
+    const user = this.userModel.findOneAndUpdate(
+      { username },
+      {
+        refreshToken: null,
+      },
+    );
+    return user;
   }
 }
