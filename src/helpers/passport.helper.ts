526209a9b8b76bb6b5d3624c9606bd35a6ef13c7
Acdaling <acdaling@gmail.com>
2021-09-03 10:43:32 +0200

    Created helper file for passport

---
+++ src/helpers/passport.helper.ts
@@ -0,0 +1,60 @@
+import passport from 'passport';
+import passportLocal from 'passport-local';
+import bcrypt from 'bcrypt';
+import { User, IUser } from '@models/user.model';
+import { NextFunction, Response, Request } from 'express';
+
+const LocalStrategy = passportLocal.Strategy;
+export const provideStategy = (strategyType: string) => {
+	switch (strategyType) {
+		case 'local':
+			return new LocalStrategy(
+				async (username: string, plainPassword: string, done) => {
+					try {
+						const foundUser = (
+							await User.findOne({
+								username,
+							})
+						)?.toJSON() as IUser;
+
+						if (!foundUser) {
+							return done(null, false, {
+								message: 'User does not exist',
+							});
+						}
+
+						const passwordCheck: boolean = await bcrypt.compare(
+							plainPassword,
+							foundUser.password
+						);
+
+						if (!passwordCheck) {
+							return done(null, false, {
+								message: 'Incorrect password',
+							});
+						}
+
+						const { password, ...user } = foundUser;
+
+						return done(null, user);
+					} catch (error) {
+						done(error, false);
+						console.error(error);
+					}
+				}
+			);
+	}
+};
+
+export const localStategyMiddleware = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	passport.authenticate('local')(req, res, next);
+};
+
+/**
+ * Global initializer for different passport strategies
+ */
+export async function onInitPassport() {}
