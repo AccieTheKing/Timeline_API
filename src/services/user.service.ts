df0e4b146c06cdc70dd67e3d02498e4b5923aba7
Acdaling <acdaling@gmail.com>
2021-09-17 11:46:14 +0200

    Updated the user service
    User service updated to have a cleaner implementation of the functionalities for the user

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,28 +1,55 @@
 import { IUser, User } from '@models/user.model';
 import { IServiceMethods } from '@services/interface';
 
+/**
+ * This class is for all the database functionalities for the user.
+ * The creation of this class was meant for trying to know how to work with interfaces
+ * and the use of generics.
+ *
+ * @author AccieTheKing
+ */
 export class UserService implements IServiceMethods<IUser> {
-	constructor() {}
+	private service;
+
+	constructor() {
+		this.service = User;
+	}
 
 	async findAll(): Promise<IUser[]> {
 		try {
-			return await User.find();
+			return await this.service.find();
 		} catch (error) {
 			console.error(error);
 		}
 	}
 
-	async find(id: number): Promise<IUser> {
+	async findById(id: string): Promise<IUser> {
 		try {
-			return await User.findById(id);
+			return await this.service.findById(id);
 		} catch (error) {
 			console.error(error);
 		}
 	}
 
-	async update(user: IUser): Promise<IUser> {
+	async findOne(param: any): Promise<IUser> {
 		try {
-			return await User.findByIdAndUpdate(user._id, { ...user });
+			return await this.service.findOne(param);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	async findByIdAndUpdate(id: string, param: any): Promise<IUser> {
+		try {
+			return await this.service.findByIdAndUpdate(id, { ...param });
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	async findByIdAndDelete(id: string): Promise<IUser> {
+		try {
+			return await this.service.findByIdAndDelete(id);
 		} catch (error) {
 			console.error(error);
 		}
