e9e61386dd32b0b460f229ef7d53be10678aef24
Acdaling <acdaling@gmail.com>
2021-09-18 09:48:24 +0200

    Cleaned board model

--- src/models/board.model.ts
+++ src/models/board.model.ts
@@ -1,11 +1,12 @@
 import { Schema, model, Document } from 'mongoose';
 
-export interface IBoard extends Document {
+export interface IBoard {
 	_id?: string;
 	title: string;
 	numberOfMilestones: number;
 	userID: string;
 	createdAt?: Date;
+	updatedAt?: Date;
 }
 
 const schema = new Schema<IBoard>(
@@ -13,8 +14,6 @@ const schema = new Schema<IBoard>(
 		title: { type: String, required: true },
 		numberOfMilestones: { type: Number, required: true },
 		userID: { type: String, required: true },
-		createdAt: { type: Date, required: false },
-		updatedAt: { type: Date, required: false },
 	},
 	{
 		timestamps: true,
