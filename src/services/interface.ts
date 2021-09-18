f47da3308c6cd4cf5fdd26d37f3436c41047206b
Acdaling <acdaling@gmail.com>
2021-09-18 09:30:34 +0200

    Added find method to Services interface

--- src/services/interface.ts
+++ src/services/interface.ts
@@ -2,6 +2,7 @@ export interface IServiceMethods<T> {
 	findAll(): Promise<T[]>;
 	findById(id: string): Promise<T>;
 	findOne(param: any): Promise<T>;
+	find(param: any): Promise<T[]>;
 	findByIdAndDelete(id: string): Promise<T>;
 	findByIdAndUpdate(id: string, param: any): Promise<T>;
 	create(element: T): Promise<T>;
