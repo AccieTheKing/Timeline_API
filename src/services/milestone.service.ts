82deb15d81e68432d25954070b202886f3a558d6
Acdaling <acdaling@gmail.com>
2021-10-06 13:54:21 +0200

    Added Board services to add and remove milestone counter of a board

--- src/services/milestone.service.ts
+++ src/services/milestone.service.ts
@@ -1,11 +1,14 @@
+import { Board } from '@models/board.model';
 import { IMilestone, Milestone } from '@models/milestone.model';
 import { IServiceMethods } from '@services/interface';
 
 export class MilestoneService implements IServiceMethods<IMilestone> {
 	private service;
+	private boardService;
 
 	constructor() {
 		this.service = Milestone;
+		this.boardService = Board;
 	}
 
 	async find(param: any): Promise<IMilestone[]> {
@@ -27,6 +30,13 @@ export class MilestoneService implements IServiceMethods<IMilestone> {
 	async create(element: IMilestone): Promise<IMilestone> {
 		try {
 			const newMilestone = new Milestone(element);
+			let updateBoardCounter = await this.boardService.findById(
+				newMilestone.boardID
+			);
+
+			updateBoardCounter.numberOfMilestones += 1;
+			await updateBoardCounter.save();
+
 			const createdMilestone: IMilestone = await newMilestone.save();
 			return createdMilestone;
 		} catch (error) {
@@ -52,6 +62,12 @@ export class MilestoneService implements IServiceMethods<IMilestone> {
 
 	async findByIdAndDelete(id: string): Promise<IMilestone> {
 		try {
+			const milestone = await this.service.findById(id);
+			let updateBoardCounter = await this.boardService.findById(
+				milestone.boardID
+			);
+			updateBoardCounter.numberOfMilestones -= 1;
+			await updateBoardCounter.save();
 			return this.service.findByIdAndDelete(id);
 		} catch (error) {
 			console.log(error);
