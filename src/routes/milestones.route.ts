34a7f1d95085e94f6feea67890f11b6d89d54732
Acdaling <acdaling@gmail.com>
2021-10-06 14:09:42 +0200

    Added middleware to milestone endpoint
    - Refactored router and removed old endpoints

--- src/routes/milestones.route.ts
+++ src/routes/milestones.route.ts
@@ -1,7 +1,13 @@
 import { Router, Request, Response } from 'express';
-import { Board } from '@models/board.model';
-import { Milestone } from '@models/milestone.model';
 import { Handler, Method, Route } from './types';
+import { checkIfAuthenticated } from '@middlewares/auth.middleware';
+import {
+	fetchMilestoneMiddleware,
+	fetchMilestoneOfBoardMiddleware,
+	createMilestoneMiddleware,
+	updateMilestoneMiddleware,
+	deleteMilestoneMiddleware,
+} from '@middlewares/milestone.middleware';
 
 const milestoneRouter = Router();
 
@@ -24,31 +30,31 @@ const routes: Route[] = [
 	{
 		path: '/',
 		method: Method.GET,
-		middleware: [],
+		middleware: [checkIfAuthenticated, fetchMilestoneMiddleware],
 		handler: milestoneHandler(`Succesfully fetched milestones`),
 	},
 	{
 		path: '/:boardID',
 		method: Method.GET,
-		middleware: [],
+		middleware: [checkIfAuthenticated, fetchMilestoneOfBoardMiddleware],
 		handler: milestoneHandler(`Succesfully fetched milestones`),
 	},
 	{
 		path: '/',
 		method: Method.POST,
-		middleware: [],
+		middleware: [checkIfAuthenticated, createMilestoneMiddleware],
 		handler: milestoneHandler(`Succesfully created milestone`),
 	},
 	{
 		path: '/:id',
 		method: Method.PATCH,
-		middleware: [],
+		middleware: [checkIfAuthenticated, updateMilestoneMiddleware],
 		handler: milestoneHandler(`Succesfully updated milestone`),
 	},
 	{
 		path: '/:id',
 		method: Method.DELETE,
-		middleware: [],
+		middleware: [checkIfAuthenticated, deleteMilestoneMiddleware],
 		handler: milestoneHandler(`Succesfully deleted milestone`),
 	},
 ];
@@ -65,78 +71,4 @@ routes.forEach((route: Route) => {
 	milestoneRouter[method](path, middleware, handler);
 });
 
-milestoneRouter.get('/', async (req, res) => {
-	try {
-		const story = await Milestone.find();
-		res.json(story);
-	} catch (err) {
-		res.status(400).json(`Error: ${err}`);
-	}
-});
-
-milestoneRouter.get('/:boardID', async (req, res) => {
-	try {
-		const story = await Milestone.find({ boardID: req.params.boardID });
-		res.json(story);
-	} catch (err) {
-		res.status(400).json(`Error: ${err}`);
-	}
-});
-
-milestoneRouter.post('/', async (req, res) => {
-	try {
-		const newStory = new Milestone({
-			title: req.body.title,
-			startDate: req.body.date.start,
-			endDate: req.body.date.end,
-			actualSituation: req.body.comment.actual,
-			desiredSituation: req.body.comment.desired,
-			boardID: req.body.boardID,
-		});
-
-		const updateBoardCounter = await Board.findById(newStory.boardID);
-		updateBoardCounter.numberOfMilestones += 1;
-
-		await updateBoardCounter.save();
-		await newStory.save();
-		const stories = await Milestone.find();
-		res.status(201).json(stories);
-	} catch (err) {
-		res.status(400).json(`Error: ${err}`);
-	}
-});
-
-milestoneRouter.post('/update/:id', async (req, res) => {
-	try {
-		const story = await Milestone.findById(req.params.id);
-
-		const responseStory = req.body.story;
-
-		story.title = responseStory.title;
-		story.startDate = responseStory.startDate;
-		story.endDate = responseStory.endDate;
-		story.actualSituation = responseStory.actualSituation;
-		story.desiredSituation = responseStory.desiredSituation;
-		await story.save();
-
-		res.json(story);
-	} catch (err) {
-		res.status(400).json(`Error: ${err}`);
-	}
-});
-
-milestoneRouter.delete('/:id', async (req, res) => {
-	try {
-		const milestone = await Milestone.findById(req.params.id);
-		const updateBoardCounter = await Board.findById(milestone.boardID);
-		updateBoardCounter.numberOfMilestones -= 1;
-
-		await milestone.remove();
-		await updateBoardCounter.save();
-		res.json('Milestone successfuly deleted');
-	} catch (err) {
-		res.status(400).json(`Error: ${err}`);
-	}
-});
-
-module.exports = router;
+export { milestoneRouter };
